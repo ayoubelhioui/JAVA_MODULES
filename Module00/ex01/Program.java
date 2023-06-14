92983ef5326558d778d621878e5301a9393111f9
Ayoub Elhioui <Elhioui001@gmail.com>
2023-06-14 20:48:43 +0100

    working on exercise 01 which about prime number

---
+++ Module00/ex01/Program.java
@@ -0,0 +1,43 @@
+package ex01;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Program{
+    public static int readFromUser(){
+        int enteredNumber = 0;
+        System.out.print("-> ");
+        Scanner sc = new Scanner(System.in);
+        enteredNumber = sc.nextInt();
+        return (enteredNumber);
+    }
+
+    public static void isPrimeNumber(int enteredNumber){
+        int iterations = 0;
+        int sqrtN = (int) Math.sqrt(enteredNumber);
+        for (int i = 2; i <= sqrtN; i++) {
+            iterations++;
+            if (enteredNumber % i == 0) {
+                System.out.println("false " + iterations);
+                return ;
+            }
+        }
+        System.out.println("true " + iterations + 1);
+    }
+
+    public static void main(String[] args){
+        int enteredNumber = 0;
+        try{
+            enteredNumber = readFromUser();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Enter a valid number");
+        }
+        if (enteredNumber <= 1)
+        {
+            System.out.println("IllegalArgument");
+            System::exit();
+        }
+        isPrimeNumber(enteredNumber);
+    }
+}
