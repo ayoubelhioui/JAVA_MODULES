1bd7cf4988b36d7074703c87375ecd01834e1dda
Ayoub Elhioui <Elhioui001@example.com>
2023-06-15 14:55:41 +0100

    the ex03 contains some parsing, and a few Allowed functions

--- Module00/ex03/Program.java
+++ Module00/ex03/Program.java
@@ -3,14 +3,31 @@ package ex03;
 import java.util.Scanner;
 
 class Program{
-    public static int readInputAndCount(){
-        int enteredNumber = 0;
-        while (enteredNumber != 42)
+    private static int weeksIndex = 1;
+    final private static Scanner standardInputScanner = new Scanner(System.in);
+    public static void readWeek(){
+        System.out.print("-> ");
+        String enteredWeek = standardInputScanner.next();
+        System.out.println(enteredWeek + " --- Week " + weeksIndex);
+        if (!enteredWeek.equals("Week " + weeksIndex))
         {
-            System.out.print("-> ");
-            Scanner sc = new Scanner(System.in);
-            enteredNumber = sc.nextInt();
+            System.out.println("IllegalArgument");
+            System.exit(-1);
+        }
+        weeksIndex++;
+    }
+    public static int readInputAndCount(){
+        int enteredNumber = 0, weeksCount = 1, testsCount = 1;
+        while (enteredNumber != 42 && weeksCount < 18)
+        {
+            readWeek();
+//            System.out.print("-> ");
+            weeksCount++;
         }
         return (0);
+    }
+
+    public static void main(String[] args){
+        readInputAndCount();
     }
 }
