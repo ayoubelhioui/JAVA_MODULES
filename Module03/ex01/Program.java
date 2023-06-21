e43666db01f73069ec4adc3e5bc7c902c5329c7b
ayoubelhioui <Elhioui001@gmail.com>
2023-06-21 18:24:26 +0100

    finishing the second exercise implementing producer-consumer module

--- Module03/ex01/Program.java
+++ Module03/ex01/Program.java
@@ -1,5 +1,15 @@
+package ex01;
+import ex01.Threads;
 class Program {
     public static void main(String[] args) {
-
+        try{
+            Threads threads = new Threads();
+            threads.startThreads();
+            threads.joinThreads();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- Module03/ex01/Threads.java
+++ Module03/ex01/Threads.java
@@ -3,16 +3,18 @@ package ex01;
 import java.util.Scanner;
 
 class Threads{
+    private static Scanner standardInputScanner;
     private Thread eggThread;
     private Thread henThread;
-    private int sharedResource;
-    private static Scanner standardInputScanner;
     private int printingCount;
+    private boolean sharedResource;
+    private Object monitor;
 
     public Threads() {
+        this.monitor = new Object();
         standardInputScanner = new Scanner(System.in);
         this.printingCount = 0;
-        this.sharedResource = 0; // 0 stands for EGG thread
+        this.sharedResource = false;
     }
 
     private void readInput() {
@@ -20,11 +22,50 @@ class Threads{
         this.printingCount = standardInputScanner.nextInt();
     }
 
+    private void printThreadMessage(String threadMessage) throws InterruptedException {
+        System.out.println(threadMessage);
+    }
+
+    public void joinThreads() throws InterruptedException {
+        this.eggThread.join();
+        this.henThread.join();
+    }
+
     private void start() throws InterruptedException {
         this.eggThread = new Thread(() -> {
-
+            for (int i = 0; i < printingCount / 2; i++)
+            {
+                synchronized (monitor) {
+                    try {
+                        if (this.sharedResource == false)
+                            monitor.wait();
+                        printThreadMessage("Hen");
+                        this.sharedResource = false;
+                        monitor.notify();
+                    } catch (InterruptedException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
         });
         this.henThread = new Thread(() -> {
+            for (int i = 0; i < printingCount / 2; i++)
+            {
+                synchronized (monitor){
+                    try{
+                        if (this.sharedResource == true)
+                            monitor.wait();
+                        printThreadMessage("Egg");
+                        this.sharedResource = true;
+                        monitor.notify();
+                    }
+                    catch(InterruptedException e)
+                    {
+                        System.out.println(e.getMessage());
+                    }
+                    monitor.notify();
+                }
+            }
 
         });
         this.eggThread.start();
