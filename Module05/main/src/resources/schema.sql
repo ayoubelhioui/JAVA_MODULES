a3f9109d913bee32f920e31ea3047dd2f9cff918
ayoubelhioui <Elhioui001@gmail.com>
2023-06-30 17:08:11 +0100

    creating tables is done, moving to the insertion part

--- Module05/main/src/resources/schema.sql
+++ Module05/main/src/resources/schema.sql
@@ -1,15 +1,23 @@
-CREATE TABLE User
+CREATE TABLE IF NOT EXISTS "User"
 (
      id SERIAL PRIMARY KEY,
      login VARCHAR(50) NOT NULL,
-     password VARCHAR(50) NOT NULL,
-     CONSTRAINT FK_user_rooms FOREIGN KEY (id)
-         REFERENCES ChatRooms(id)
+     password VARCHAR(50) NOT NULL
 )
 
-CREATE TABLE ChatRoom
+CREATE TABLE IF NOT EXISTS ChatRoom
 (
-    id SEIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-
+    user_id INTEGER REFERENCES "User" (id)
 )
+
+CREATE TABLE IF NOT EXISTS Messages
+(
+    id SERIAL PRIMARY KEY,
+    text VARCHAR(200) NOT NULL,
+    date date,
+    author INTEGER REFERENCES "User" (id),
+    room INTEGER REFERENCES ChatRoom (id)
+)
+
