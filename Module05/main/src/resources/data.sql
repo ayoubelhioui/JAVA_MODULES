2cae5eb3105d787f2ac282fc66192f01ce58765f
ayoubelhioui <Elhioui001@gmail.com>
2023-06-30 18:51:40 +0100

    inserting data into tables is done

---
+++ Module05/main/src/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO "User" (login, password)
+VALUES ('ael-hiou', '123456');
+INSERT INTO "User" (login, password)
+VALUES ('klaarous', '1234569834');
+INSERT INTO "User" (login, password)
+VALUES ('hnaciri', 'ksdjf33');
+INSERT INTO "User" (login, password)
+VALUES ('mel-amma', '12io12d');
