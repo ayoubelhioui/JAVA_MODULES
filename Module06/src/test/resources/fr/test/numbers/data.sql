13355a94aaa0d449b4da2401d6d4e4df4e9d7a1e
Ayoub Elhioui <Elhioui001@gmail.com>
2023-07-27 18:46:13 +0100

    working with HyperSql as an embeddedDatabase

---
+++ Module06/src/test/resources/fr/test/numbers/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Product (name, price)
+VALUES ('ael-hiou', '123456');
+INSERT INTO Product (name, price)
+VALUES ('klaarous', '1234569834');
+INSERT INTO Product (name, price)
+VALUES ('hnaciri', '511');
+INSERT INTO Product (name, price)
+VALUES ('mel-amma', '784');
---
+++ Module06/src/test/resources/fr/test/numbers/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS Product
+(
+    id SERIAL PRIMARY KEY,
+    name varchar(50),
+    price varchar(50),
+)
