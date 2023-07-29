e8a360de4fe1302f6886448f607e917910dd9c82
Ayoub Elhioui <Elhioui001@gmail.com>
2023-07-29 10:20:55 +0100

    using mockito to test some units

--- Module06/src/test/java/fr/test/services/UserServiceImplTest.java
+++ Module06/src/test/java/fr/test/services/UserServiceImplTest.java
@@ -22,26 +22,51 @@ class UserServiceImplTest {
     public void authenticateForCorrectTest() throws EntityNotFoundException, AlreadyAuthenticatedException {
         User user = new User(1, "helloWorld", "sdjf", false);
         User user1 = new User(2, "imad", "dtrab", false);
+        User user2 = new User(2, "biba", "khawa", false);
         when(userRepository.findByLogin("helloWorld")).thenReturn(user);
-        verify(this.userRepository, times(1)).findByLogin("helloWorld");
+        when(userRepository.findByLogin("imad")).thenReturn(user1);
+        when(userRepository.findByLogin("biba")).thenReturn(user2);
         boolean isAuthenticate = this.userService.authenticate("helloWorld", "sdjf");
-//        boolean isAuthenticate2 = this.userService.authenticate("imad", "dtrab");
+        boolean isAuthenticate2 = this.userService.authenticate("imad", "dtrab");
+        boolean isAuthenticate3 = this.userService.authenticate("biba", "khawa");
+        verify(this.userRepository, times(1)).findByLogin("helloWorld");
+        verify(this.userRepository, times(1)).findByLogin("imad");
+        verify(this.userRepository, times(1)).findByLogin("biba");
         assertTrue(isAuthenticate);
-//        assertTrue(isAuthenticate2);
+        assertTrue(isAuthenticate2);
+        assertTrue(isAuthenticate3);
     }
 
     @Test
     public void authenticateForIncorrectLogin() throws EntityNotFoundException {
         when(userRepository.findByLogin("helalo")).thenThrow(EntityNotFoundException.class);
         assertThrows(EntityNotFoundException.class, ()-> this.userService.authenticate("helalo", "helloWorld"));
+        verify(this.userRepository, times(1)).findByLogin("helalo");
+        when(userRepository.findByLogin("imad")).thenThrow(EntityNotFoundException.class);
+        assertThrows(EntityNotFoundException.class, ()-> this.userService.authenticate("imad", "helloWorld"));
+        verify(this.userRepository, times(1)).findByLogin("imad");
+        when(userRepository.findByLogin("younes")).thenThrow(EntityNotFoundException.class);
+        assertThrows(EntityNotFoundException.class, ()-> this.userService.authenticate("younes", "helloWorld"));
+        verify(this.userRepository, times(1)).findByLogin("younes");
+
     }
 
-//    @Test
-//    public void authenticateForIncorrectPassword() throws EntityNotFoundException, AlreadyAuthenticatedException {
-//        User user = new User(1, "helloWorld", "sdjf", false);
-//        when(userRepository.findByLogin("helloWorld")).thenReturn(user);
-//        boolean isAuthenticate = this.userService.authenticate("helloWorld", "incorrectPassword");
-//        verify(this.userRepository, times(1)).findByLogin("helloWorld");
-//        assertFalse(isAuthenticate);
-//    }
+    @Test
+    public void authenticateForIncorrectPassword() throws EntityNotFoundException, AlreadyAuthenticatedException {
+        User user = new User(1, "helloWorld", "sdjf", false);
+        User user1 = new User(2, "imad", "dtrab", false);
+        User user2 = new User(2, "biba", "khawa", false);
+        when(userRepository.findByLogin("helloWorld")).thenReturn(user);
+        when(userRepository.findByLogin("imad")).thenReturn(user1);
+        when(userRepository.findByLogin("biba")).thenReturn(user2);
+        boolean isAuthenticate = this.userService.authenticate("helloWorld", "sd");
+        boolean isAuthenticate2 = this.userService.authenticate("imad", "dc");
+        boolean isAuthenticate3 = this.userService.authenticate("biba", "cv");
+        verify(this.userRepository, times(1)).findByLogin("helloWorld");
+        verify(this.userRepository, times(1)).findByLogin("imad");
+        verify(this.userRepository, times(1)).findByLogin("biba");
+        assertFalse(isAuthenticate);
+        assertFalse(isAuthenticate2);
+        assertFalse(isAuthenticate3);
+    }
 }
